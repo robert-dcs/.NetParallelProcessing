18c6679d8a755a7ca5ee14904f62e6e341a00ab8
Robertson Souza <robertson.dcs@gmail.com>
2023-06-29 14:56:24 -0300

    adding more algorithms for analysis

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace LerPlanilhaExcel
     {
         static void Main(string[] args)
         {
-            var xls = new XLWorkbook(@"C:\Users\Robertson\Documents\projetos\rt_iot\dotNeParalelismProcessor\sample\data1000.xlsx");
+            int sampleSize = 1000000;
+            var xls = new XLWorkbook(@"C:\Users\Robertson\Documents\projetos\rt_iot\dotNeParalelismProcessor\sample\data"+sampleSize+".xlsx"); 
             var file = xls.Worksheets.First(w => w.Name == "Sheet1");
             var totalRows = file.Rows().Count();
             var listOfPeople = new List<string>();
@@ -18,12 +19,15 @@ namespace LerPlanilhaExcel
             }
             Console.WriteLine("First record from sample: " + listOfPeople[0]);
             Console.WriteLine("Las record from sample: " + listOfPeople[listOfPeople.Count() - 1]);
-            synchronousProcessing(listOfPeople);
-            parallelProcessing(listOfPeople);
+               
+            SynchronousProcessing(listOfPeople);
+            ParallelProcessing(listOfPeople);
+            ParallelProcessing2(listOfPeople);
+            ParallelProcessing3(listOfPeople);
         }
 
-
-        static void parallelProcessing(List<string> listOfPeople) {
+        static void ParallelProcessing(List<string> listOfPeople) 
+        {
             var dataBaseCon = new DBconnection.Database();
             using var con = dataBaseCon.getConnection();
             dataBaseCon.dropAndCreateTable();
@@ -34,11 +38,67 @@ namespace LerPlanilhaExcel
             });
             watch.Stop();
             var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine("[C#] Parallel implementation took " + elapsedMs + " milliseconds.");
+            Console.WriteLine("[C#] Parallel implementation 1 took " + elapsedMs + " milliseconds.");
             Console.WriteLine("[C#] Processed " + listOfPeople.Count() + " records.");
         }
 
-        static void synchronousProcessing(List<string> listOfPeople) {
+        static void ParallelProcessing2(List<string> listOfPeople) 
+        {
+            var dataBaseCon = new DBconnection.Database();
+            using var con = dataBaseCon.getConnection();
+            dataBaseCon.dropAndCreateTable();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            Parallel.ForEach(listOfPeople, dataBaseCon.inserPersonToDB);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("[C#] Parallel implementation 2 took " + elapsedMs + " milliseconds.");
+            Console.WriteLine("[C#] Processed " + listOfPeople.Count() + " records.");
+        }
+
+        static void ParallelProcessing3(List<string> listOfPeople) 
+        {
+            var dataBaseCon = new DBconnection.Database();
+            List<Thread> threads = new List<Thread>();
+            using var con = dataBaseCon.getConnection();
+            dataBaseCon.dropAndCreateTable();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            int maxConcurrency =  Environment.ProcessorCount;
+            SemaphoreSlim semaphore = new SemaphoreSlim(maxConcurrency);
+
+            foreach (string person in listOfPeople)
+            {
+                Thread thread = new Thread(() =>
+                {
+                    semaphore.Wait();
+
+                    try
+                    {
+                        dataBaseCon.inserPersonToDB(person);
+                    }
+                    finally
+                    {
+                        semaphore.Release(); 
+                    }
+                });
+
+                thread.Start();
+                threads.Add(thread); 
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("[C#] Parallel implementation 3 took " + elapsedMs + " milliseconds.");
+            Console.WriteLine("[C#] Processed " + listOfPeople.Count() + " records.");
+        }
+        
+        static void SynchronousProcessing(List<string> listOfPeople) 
+        {
             var dataBaseCon = new DBconnection.Database();
             using var con = dataBaseCon.getConnection();
             dataBaseCon.dropAndCreateTable();
